f7923a75dcfb037bf578adfbabd11e093c52df89
Rishabh Shukla <rishabhshukla321@gmail.com>
2019-02-19 13:10:02 +0530

    init commit

---
+++ firstpy.py
@@ -0,0 +1,16 @@
+def fact(n):
+    if(n==0 or n==1):
+        return 1
+    else:
+        return(n*fact(n-1))
+
+x=int(input("Enter a number whose factorial you want to find:"))
+factorial=fact(x)
+print("Factorial of %d is : %d"%(x,factorial)) 
+
+if(factorial==120):
+    myfile=open('batman.txt')
+    print(myfile.read())
+else:
+    for i in range(1,11):
+        print("%d * %d = %d"%(x,i,x*i))
